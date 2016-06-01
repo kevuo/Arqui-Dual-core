e8ad092b4b0a7b80ae25bcd7cd5f04e7924fbdfe
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2016-06-01 00:26:47 -0600

    Nuevos archivos api

--- Qt/Proyecto2/main.cpp
+++ Qt/Proyecto2/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
+#include "api.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    API api;
+    api.writeDMA();
+    //QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
 
-    return a.exec();
+    //return a.exec();
 }
