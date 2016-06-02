fac2e2383947f7334f1c132befc2ba7dde3dc2f8
Miguel Taylor <mtlazul@gmail.com>
2016-06-02 14:46:15 -0600

    bajando en el filtro

--- Filtro/admimagen.h
+++ Filtro/admimagen.h
@@ -1,5 +1,6 @@
 #ifndef ADMIMAGEN_H
 #define ADMIMAGEN_H
+
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <stdio.h>
@@ -20,19 +21,15 @@ public:
     int mGet_Width();
     int mGet_Channels();
     int mGet_Pixel_Size();
-    void mProcesar_Imagen(int pCant_Div);
+    void mProcesar_Imagen();
     cv::Mat mReconstruir();
     cv::Mat mReconstruir(cv::Mat pMatriz);
     bool mImage_Null();
 
 private:
 
-    void mTile_Filling(int pCant_Div);
-    void mGet_Sub_Image(int pWidth, int pHeight);
-    void mInterpolation_NN();
-    void mInterpolation_Taylor();
-    void mInterpreter();
     cv::Mat aImagen;
+    int** aMatrix;
     bool aImageNull;
     int aHeight;
     int aWidth;
--- Filtro/filtro.cpp
+++ Filtro/filtro.cpp
@@ -1,24 +1,61 @@
+#include "filtro.h"
 
-#include <iostream>
-#include <fstream>
-using namespace std;
+Filtro::Filtro() {
+    aN = 10; //filas
+    aM = 10; //columnas
+    aMatrix1 = new int*[aN];
+    aMatrix2 = new int*[aN];
+    for (int i = 0; i < aN; i++) {
+        aMatrix1[i] = new int[aM];
+        aMatrix2[i] = new int[aM];
+    }
 
-int max5n (int a,int b,int c,int d,int e){
-    int max = (a<b)? b:a;
-    max = (max<c)? c:max;
-    max = (max<d)? d:max;
-    return (max<e)? e:max;
+    mSalt_Filter();
+    mTraspuesta();
+    mSalt_Filter();
+    mTraspuesta();
+
+    mPrint_Matrix(aMatrix1, aN, aM);
+    mPrint_Matrix(aMatrix2, aN, aM);
+
+    for (int i = 0; i < aN; i++) {
+        delete [] aMatrix1[i];
+        delete [] aMatrix2[i];
+    }
+    delete [] aMatrix1;
+    delete [] aMatrix2;
 }
 
-void saltFilter(int **matrix, int N, int M, int **result) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 2; j < M-2; j++) {
-            result[i][j] = max5n(matrix[i][j-2],matrix[i][j-1],matrix[i][j],matrix[i][j+1],matrix[i][j+2]);
-        }
+Filtro::Filtro(int** matrix, int N, int M) {
+    aN = N; //filas
+    aM = M; //columnas
+    aMatrix1 = matrix;
+    aMatrix2 = new int*[aN];
+    for (int i = 0; i < aN; i++) {
+        aMatrix2[i] = new int[aM];
     }
 }
 
-void printMatrix(int **matrix, int N, int M) {
+Filtro::mSet_Matrix(int **Matrix) {
+    aMatrix1 = Matrix;
+    aMatrix2 = new int*[aN];
+    for (int i = 0; i < aN; i++) {
+        aMatrix2[i] = new int[aM];
+    }
+}
+
+Filtro::mGet_Matrix() {
+    return aMatrix1;
+}
+
+Filtro::mProcesar_Imagen(){
+    mSalt_Filter();
+    mTraspuesta();
+    mSalt_Filter();
+    mTraspuesta();
+}
+
+Filtro::mPrint_Matrix(int **matrix, int N, int M) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cout << matrix[i][j] << "\t";
@@ -27,29 +64,25 @@ void printMatrix(int **matrix, int N, int M) {
     }
 }
 
-int main() {
-    int N, M;
-    int **Matrix, **Temporal;
-
-    N = 10;//filas
-    M = 10;//columnas
-    Matrix = new int*[N];
-    Temporal = new int*[N];
-    for (int i = 0; i < N; i++) {
-        Matrix[i] = new int[M];
-        Temporal[i] = new int[M];
-    }
-
-    saltFilter (Matrix,5,M,Temporal);
-    printMatrix(Matrix, N, M);
-    printMatrix(Temporal, N, M);
-
-    for (int i = 0; i < N; i++) {
-        delete [] Matrix[i];
-        delete [] Temporal[i];
-    }
-    delete [] Matrix;
-    delete [] Temporal;
+Filtro::mMax_5n(int a, int b, int c, int d, int e) {
+    int max = (a < b) ? b : a;
+    max = (max < c) ? c : max;
+    max = (max < d) ? d : max;
+    return (max < e) ? e : max;
 }
 
+Filtro::mSalt_Filter() {
+    for (int i = 0; i < aN; i++) {
+        for (int j = 2; j < aM - 2; j++) {
+            aMatrix2[i][j] = mMax_5n(matrix[i][j - 2], aMatrix1[i][j - 1], aMatrix1[i][j], aMatrix1[i][j + 1], aMatrix1[i][j + 2]);
+        }
+    }
+}
 
+Filtro::mTraspuesta() {
+    for (int i = 0; i < aN; i++) {
+        for (int j = 2; j < aM - 2; j++) {
+            aMatrix1[i][j] = aMatrix2[j][i];
+        }
+    }
+}
