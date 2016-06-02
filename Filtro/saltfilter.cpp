a7f93f63cb49e04a181fe11596501b7783604407
Miguel Taylor <mtlazul@gmail.com>
2016-06-02 14:47:26 -0600

    add ui

---
+++ Filtro/saltfilter.cpp
@@ -0,0 +1,56 @@
+#include "saltfilter.h"
+#include "ui_saltfilter.h"
+
+
+
+SaltFilter::SaltFilter(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::SaltFilter)
+{
+    ui->setupUi(this);
+}
+
+SaltFilter::~SaltFilter()
+{
+    delete ui;
+}
+
+void SaltFilter::on_actionAbrir_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+          tr("Abrir Imagen"), "/home/", tr("Image Files (*.png *.jpg *.bmp)"));
+    string File = fileName.toStdString();
+    if (fileName.size()==0)return;
+    aAdmImage=AdmImagen(File);
+    aAdmMatriz=AdmMatriz();
+    QPixmap Aux(fileName);
+    ui->ImageShow->setPixmap(Aux);
+    ui->ImageShow->setAlignment(0);
+}
+
+void SaltFilter::on_Procesar_pressed()
+{
+    if (aAdmImage.mImage_Null()){
+        aAdmImage=AdmImagen("Qpixmap.jpg");
+    }
+    //aAdmImage.mProcesar_Imagen((int)ui->CantDiv->value());
+    aAdmImage.mProcesar_Imagen();
+    cv::imwrite("Qpixmap.jpg",aAdmImage.mReconstruir());
+    QPixmap Aux("Qpixmap.jpg");
+    ui->ImageShow->setPixmap(Aux);
+    ui->ImageShow->setAlignment(0);
+    ui->actionGuardar->setEnabled(true);
+}
+
+void SaltFilter::on_actionSalir_triggered()
+{
+    this->destroy(true,true);
+}
+
+void SaltFilter::on_actionGuardar_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+          tr("Guardar XML"), "/home/", tr("XML Files (*.xml)"));
+    if (fileName.size()==0)return;
+    this->aAdmImage.mGuardar_Img(fileName);
+}
