518820827fcadebd971b7e8d3ad9a8b76c15908b
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2016-06-01 18:05:19 -0600

    Se inicializan las variables del PCIE.h

--- Qt/Proyecto2/Interfaz.c
+++ Qt/Proyecto2/Interfaz.c
@@ -1,10 +1,18 @@
 #include "stdio.h"
 #include "PCIE.h"
 
-
 int iniciarPCIe()
 {
     printf ("---------- Iniciando el PCIe de la memoria ----------\n");
+
+    void *lib_handle;
+    PCIE_HANDLE hPCIE;
+    int nActiveFrame = 0;
+    BOOL bInvert = FALSE;
+    BOOL bSuccess;
+
     printf ("---------- Cargando el PCIe ----------\n");
     return 0;
 }
+
+
