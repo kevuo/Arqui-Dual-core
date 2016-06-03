1c33d424360fd36eb33646574ea17ae6ee596330
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2016-06-03 12:10:08 -0600

    Adaptacion de las entradas con los valores que recibe el API

--- Qt/Proyecto2/api.h
+++ Qt/Proyecto2/api.h
@@ -1,12 +1,14 @@
 #ifndef API_H
 #define API_H
 
+#include "PCIE.h"
+
 extern "C" {
-    void DMAWriteMatriz();
-    void DMAReadMatriz();
-    void DMAWriteDimensiones();
-    void ReadNiosStatus();
-    void WriteDataReadyStatus();
+    void  DMAWriteMatriz(PCIE_LOCAL_ADDRESS LocalAddress, void* pData, DWORD dwDataSize);
+    void* DMAReadMatriz(PCIE_LOCAL_ADDRESS LocalAddress, DWORD dwBufSize);
+    void  DMAWriteDimensiones(PCIE_LOCAL_ADDRESS LocalAdress, void* pData, DWORD dwDataSize);
+    unsigned char* ReadNiosStatus(PCIE_ADDRESS PcieAddress);
+    void WriteDataReadyStatus(PCIE_ADDRESS PcieAddress, DWORD dwData);
     //int hola(int zumbi);
 }
 
