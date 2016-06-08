2b3b1ca883ad0cdaaf8973f18c580052f9475415
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2016-06-07 20:53:43 -0600

    Agregando mis archivos modificados del PCIe

---
+++ API/api.h
@@ -0,0 +1,25 @@
+#ifndef API_H
+#define API_H
+
+#include "PCIE.h"
+
+extern "C" {
+    void DMAWriteMatriz(void* pData, DWORD dwDataSize);
+    void* DMAReadMatriz(DWORD dwBufSize, void *pBuffer);
+    void DMAWriteDimensiones(void* pData, DWORD dwDataSize);
+    unsigned char* ReadNiosStatus(unsigned char* pdwData);
+    void WriteDataReadyStatus(DWORD dwData);
+    //int hola(int zumbi);
+}
+
+class API{
+    public:
+        API();
+        void escribirMatriz(void* pData, unsigned int dwDataSize);
+        void* leerMatriz(unsigned int dwBufSize, void* pBuffer);
+        void escribirDimensiones(void *pData, unsigned int dwDataSize);
+        unsigned char* obtenerNiosStatus(unsigned char* pdwData);
+        void escribirEstadoDatoListo(unsigned int dwData);
+};
+
+#endif // API_H
